3ea1b9337465c8b78bd0859d54358ebeb5083a41
Lucas Rodrigues <lucas.carvalho.rodrigues@gmail.com>
2016-02-19 23:15:36 -0200

    Change gulp steps to have DEV and DEPLOY process. Configuration of ava to test code

--- gulpFile.js
+++ gulpFile.js
@@ -4,11 +4,15 @@ const del = require('del');
 const runSequence = require('run-sequence');
 const uglify = require('gulp-uglify');
 
-const TASK_ES6_TO_ES5 = 'TASK_ES6_TO_ES5';
+const TASK_ES6_TO_ES5_TEST = 'TASK_ES6_TO_ES5_TEST';
+const TASK_ES6_TO_ES5_SRC = 'TASK_ES6_TO_ES5_SRC';
 const TASK_CLEAN_DIST = 'TASK_CLEAN_DIST';
-const BUILD = 'BUILD';
+const BUILD_DEVELOPMENT = 'BUILD_DEVELOPMENT';
+const BUILD_DEPLOY = 'BUILD_DEPLOY';
+const TASK_ES6_TO_ES5_SRC_DEPLOY = 'TASK_ES6_TO_ES5_SRC_DEPLOY';
 
-const SOURCE_FOLDER = 'src/';
+const SOURCE_FOLDER_SRC = 'src/';
+const SOURCE_FOLDER_TEST = 'test/';
 const DESTINATION_FOLDER = 'lib/';
 
 gulp.task(TASK_CLEAN_DIST, function () {
@@ -16,19 +20,49 @@ gulp.task(TASK_CLEAN_DIST, function () {
   del.sync([DESTINATION_FOLDER + '**']);
 });
 
-gulp.task(TASK_ES6_TO_ES5, function () {
+gulp.task(TASK_ES6_TO_ES5_SRC_DEPLOY, function () {
 
   const babelConfiguration = {
     presets: ['es2015']
   };
 
-  return gulp.src(SOURCE_FOLDER + '**/*.js')
+  return gulp.src(SOURCE_FOLDER_SRC + '**/*.js')
     .pipe(gulpBabel(babelConfiguration))
     .pipe(uglify())
     .pipe(gulp.dest(DESTINATION_FOLDER));
 });
 
-gulp.task(BUILD,function(){
 
-  runSequence(TASK_CLEAN_DIST,TASK_ES6_TO_ES5);
+gulp.task(TASK_ES6_TO_ES5_SRC, function () {
+
+  const babelConfiguration = {
+    presets: ['es2015']
+  };
+
+  return gulp.src(SOURCE_FOLDER_SRC + '**/*.js')
+    .pipe(gulpBabel(babelConfiguration))
+    .pipe(uglify())
+    .pipe(gulp.dest(DESTINATION_FOLDER + '/src'));
+});
+
+gulp.task(TASK_ES6_TO_ES5_TEST, function () {
+
+  const babelConfiguration = {
+    presets: ['es2015']
+  };
+
+  return gulp.src(SOURCE_FOLDER_TEST + '**/*.js')
+    .pipe(gulpBabel(babelConfiguration))
+    .pipe(uglify())
+    .pipe(gulp.dest(DESTINATION_FOLDER + '/test'));
+});
+
+gulp.task(BUILD_DEPLOY,function(){
+
+  runSequence(TASK_CLEAN_DIST,TASK_ES6_TO_ES5_SRC_DEPLOY);
+});
+
+gulp.task(BUILD_DEVELOPMENT,function(){
+
+  runSequence(TASK_CLEAN_DIST,TASK_ES6_TO_ES5_SRC,TASK_ES6_TO_ES5_TEST);
 });
